2878d5af1dee55017290856523a89397003cccf4
Daniel Gallichan <dgallichan@yahoo.com>
2021-05-29 20:28:37 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,19 @@
+input.onButtonPressed(Button.AB, function () {
+    buttonpress = 1
+})
+let buttonpress = 0
+buttonpress = 0
 radio.setGroup(99)
 bitbot.select_model(BBModel.Classic)
 bitbot.bbEnableBluetooth(BBBluetooth.btEnable)
+control.waitForEvent(buttonpress, 1)
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
 bitbot.go(BBDirection.Forward, 60)
 bitbot.ledRainbow()
 control.waitMicros(4000000)
@@ -9,7 +22,7 @@ bitbot.ledShift()
 control.waitMicros(4000000)
 basic.showLeds(`
     # # # # #
-    # # # # #
+    # . # . #
     # # # # #
     # # . # #
     # # # # #
